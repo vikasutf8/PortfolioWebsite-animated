4081816686e123f41c6324d77ab62a998243c87e
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-02-15 22:54:37 +0530

    intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/sections/Header"
 import Hero from "@/sections/Hero"
+import Intro from "@/sections/Intro"
 
 export default function Home() {
   return (
     <>
     <Header/>
     <Hero/>
+    <Intro/>
     </>
   )
 }
--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 
 const Intro: FC = () => {
-  return <div>Intro</div>;
+  return <section className="py-24 md:py-32 lg:py-40 mt-12 md: mt-16 lg:mt-20">
+    <div className="container">
+      <h2 className="text-4xl md:text-6xl lg:text-7xl lg:w-[80%]">
+      A Full-Stack Software Engineer and CSE graduate from IIITDM Jabalpur (2025) with strong fundamentals, expertise in microservices & collaborate synergistically in open-source development
+      </h2>
+    </div>
+  </section>
 };
 
 export default Intro;
