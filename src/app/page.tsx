7f844b059a51816818d20cb1c66e8a4f607f8e31
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-02-15 23:35:15 +0530

    faqs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FAQs from "@/sections/FAQs"
 import Header from "@/sections/Header"
 import Hero from "@/sections/Hero"
 import Intro from "@/sections/Intro"
@@ -9,6 +10,7 @@ export default function Home() {
     <Hero/>
     <Intro/>
     <Projects/>
+    <FAQs/>
     </>
   )
 }
--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -1,3 +1,4 @@
+import { div } from "motion/react-client";
 import { FC } from "react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
@@ -25,7 +26,28 @@ const faqs = [
 ];
 
 const FAQs: FC = () => {
-  return <div>FAQs</div>;
+  return <section className="section">
+    <div className="container">
+      <h2 className="text-4xl md:text-7xl lg:text-8xl">FAQs</h2>
+      <div className="mt-10 md:mt-16 lg:mt-20 last:border-b">
+        {faqs.map(({ question, answer }) => (
+          <div key={question} className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py-10">
+            <div className="flex items-center justify-between gap-4">
+              {/* question text */}
+              <div className="text-2xl md:text-3xl lg:text-4xl">{question}</div>
+              {/* plus icon svg */}
+             <div className="inline-flex items-center justify-center size-11 border border-stone-400  rounded-full shrink-0">
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+             </div>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
 };
 
 export default FAQs;
