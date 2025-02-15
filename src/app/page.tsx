c9d8cc1701d576ca5c2b453bae9cd54eb6e2a5df
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-02-15 23:18:12 +0530

    project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/sections/Header"
 import Hero from "@/sections/Hero"
 import Intro from "@/sections/Intro"
-
+import Projects from "@/sections/Projects"
 export default function Home() {
   return (
     <>
     <Header/>
     <Hero/>
     <Intro/>
+    <Projects/>
     </>
   )
 }
